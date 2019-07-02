a93d52b4d53e6f3f23684433220b1a68e74e9e9f
root <root@ip-172-31-34-227.ec2.internal>
2019-07-02 10:49:02 +0000

    7th

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 clear;
 apt update -y
-apt install python3-pip -y
+python3 get-pip.py
 pip3 install ansible
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
