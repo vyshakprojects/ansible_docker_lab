fb1cd67f02b6cc216e7ab39c4fdbb9067fa3d411
root <root@ip-172-31-40-37.ec2.internal>
2019-07-02 09:58:20 +0000

    6th

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 !#/bin/bash
 clear;
 apt update -y
+apt install python3-pip -y
+pip3 install ansible
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
@@ -8,9 +10,7 @@ apt update
 apt-cache policy docker-ce
 apt install -y docker-ce
 apt-get install docker-compose -y
-apt install python3-pip -y
 clear
-pip3 install ansible
 sleep 2
 clear
 echo "PRESS ENTER FOR THE FOLLOWING PROMPTS"
